eccd13795e2364b2aeadb381e599897ac376e167
Divesh David <divesh.david@gmail.com>
2020-11-14 11:02:55 -0500

    Implicit vs Explicit Waits

--- CreditCards.UITests/CredCardAppShould.cs
+++ CreditCards.UITests/CredCardAppShould.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CreditCards.UITests
 {
@@ -11,6 +12,13 @@ namespace CreditCards.UITests
         private const string ApplyUrl = "http://localhost:44108/Apply";
         private const string HomePageUrl = "http://localhost:44108/";
         private const string CreditCardPageTile = "Credit Card Application - Credit Cards";
+        private readonly ITestOutputHelper output;
+
+        public CredCardAppShould(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
 
         [Fact]
         [Trait("Category", "Application")]
@@ -142,7 +150,80 @@ namespace CreditCards.UITests
             }
         }
 
+        [Fact]
+        [Trait("Category", "Application")]
+        public void VerifyPageWithImplicitWaitFailingTest()
+        {
+            using (IWebDriver driver = new ChromeDriver("."))
+            {
+                try
+                {
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Setting Implicit Wait");
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(35);
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Navigate to Home Page");
+                    driver.Navigate().GoToUrl(HomePageUrl);
+                    DemoHelper.Pause();
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding Element");
+                    IWebElement clickApplyNowButton = driver.FindElement(By.ClassName("customer-service-apply-now"));
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Found Element Displayed:{clickApplyNowButton.Displayed} and Enabled:{clickApplyNowButton.Enabled}");
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Clicking Element");
+                    clickApplyNowButton.Click();
+                    DemoHelper.Pause();
+                }
+                catch (Exception e)
+                {
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} {e} Reason for Failure: Button is still not Enabled or displayed");
+                }
 
+            }
+
+        }
+
+        [Fact]
+        [Trait("Category", "Application")]
+        public void ExplicitWaitsUnreachableElementFailingTest()
+        {
+            using (IWebDriver driver = new ChromeDriver("."))
+            {
+                try
+                {
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Navigate to Home Page");
+                    driver.Navigate().GoToUrl(HomePageUrl);
+                    DemoHelper.Pause();
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding Element");
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(35));
+                    IWebElement clickApplyNowButton = wait.Until((d) => d.FindElement(By.ClassName("customer-service-apply-now")));
+
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Found Element Displayed:{clickApplyNowButton.Displayed} and Enabled:{clickApplyNowButton.Enabled}");
+                    output.WriteLine($"{DateTime.Now.ToLongTimeString()} Clicking Element");
+                    clickApplyNowButton.Click();
+                    DemoHelper.Pause();
+                    Assert.Equal(ApplyUrl, driver.Url);
+                    Assert.Equal(CreditCardPageTile, driver.Title);
+                }
+                catch (Exception e)
+                {
+                    output.WriteLine($"{e} Reason for Failure: Button is still not Enabled or displayed");
+                }
+            }
+
+        }
+
+        [Fact]
+        [Trait("Category", "Application")]
+        public void ExplicitWaitsWithPreBuiltFunctions()
+        {
+            using (IWebDriver driver = new ChromeDriver("."))
+            {
+                driver.Navigate().GoToUrl(HomePageUrl);
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(35));
+                IWebElement clickApply = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("customer-service-apply-now")));
+                clickApply.Click();   
+            
+            }
+            
+        }
 
     }
 }
